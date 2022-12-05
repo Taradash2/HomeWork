b7d49e6b77b80598ca4f945e1e90e7bd1b61407e
Danila <taradash98@gmail.com>
2022-12-05 17:22:20 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Напишите 1е число =");
+ int a = Convert.ToInt32(Console.ReadLine());
+
+ Console.WriteLine("Напишите 2е число =");
+ int b = Convert.ToInt32(Console.ReadLine());
+ if (a > b)
+  Console.WriteLine(a);
+  else
+  Console.WriteLine(b);
