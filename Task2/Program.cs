b6231cf445095bcd12d5eaaf8aa30780cc950331
Danila <taradash98@gmail.com>
2022-12-05 17:54:06 +0300

    task2

---
+++ Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Напишите числа: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b && a > c)
+Console.WriteLine(a);
+else if (b > a && b > c)
+Console.WriteLine(b);
+else if (c > a && c > b)
+Console.WriteLine(c);
